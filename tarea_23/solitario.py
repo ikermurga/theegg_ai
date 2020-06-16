2dfa708b1d4db972e2e1726f621859fc532cf1cb
ikermurga <ikermurga@gmail.com>
2020-06-17 00:39:30 +0200

    cifrado y descifrado

--- tarea_23/solitario.py
+++ tarea_23/solitario.py
@@ -6,14 +6,31 @@ def main():
     conversor = ConversorLetrasNumeros()
     generador = GeneradorSolitario(conversor)
 
-    texto_original = 'HOLA MUNDO'
-    ristra = generador.generar_letras_secuencia_de_clave(len(texto_original))
+    # TODO: pedir texto al usuario. antes preguntar si cifrar o descifrar
+    # hacer que el resultado se muestre en bloques de 5?
+    texto = 'HOLA MUNDO'
+    clave = generador.generar_letras_secuencia_de_clave(len(texto))
+    cifrado = cifrar(texto, clave, conversor)
+    print(cifrado)
 
-    numeros_original = lista_numerica_desde_texto(texto_original, conversor)
-    numeros_ristra = lista_numerica_desde_texto(ristra, conversor)
-    numeros_combinados = sumar_numeros(numeros_original, numeros_ristra)
+    descifrado = descifrar(cifrado, clave, conversor)
+    print(descifrado)
+
+
+def cifrar(texto, clave, conversor):
+    numeros_original = lista_numerica_desde_texto(texto, conversor)
+    numeros_clave = lista_numerica_desde_texto(clave, conversor)
+    numeros_combinados = sumar_numeros(numeros_original, numeros_clave)
     texto_cifrado = texto_desde_lista_numerica(numeros_combinados, conversor)
-    print(texto_cifrado)
+    return texto_cifrado
+
+
+def descifrar(texto, clave, conversor):
+    numeros_original = lista_numerica_desde_texto(texto, conversor)
+    numeros_clave = lista_numerica_desde_texto(clave, conversor)
+    numeros_combinados = restar_numeros(numeros_original, numeros_clave)
+    texto_cifrado = texto_desde_lista_numerica(numeros_combinados, conversor)
+    return texto_cifrado
 
 
 def texto_desde_lista_numerica(numeros, conversor):
@@ -37,6 +54,18 @@ def sumar_numeros(lista1, lista2):
     i = 0
     while i < len(lista1):
         valor = (lista1[i] + lista2[i]) % 26
+        resultado.append(valor)
+        i += 1
+    return resultado
+
+
+def restar_numeros(lista1, lista2):
+    resultado = []
+    i = 0
+    while i < len(lista1):
+        if lista1[i] < lista2[i]:
+            lista1[i] += 26
+        valor = lista1[i] - lista2[i]
         resultado.append(valor)
         i += 1
     return resultado
