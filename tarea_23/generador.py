2e15835d8a7a3feaf66f8409417d1d4497276745
ikermurga <ikermurga@gmail.com>
2020-06-18 22:44:38 +0200

    añadido comentarios

--- tarea_23/generador.py
+++ tarea_23/generador.py
@@ -2,10 +2,20 @@ from baraja import Baraja
 
 
 class GeneradorSolitario:
+    # Clase que genera la clave (la base del algoritmo del
+    # solitario) a partir de una baraja. Utiliza un objeto
+    # baraja que lleva el orden de las cartas tras cada paso
+    # y un ConversorLetrasNumeros para pasar de letras a
+    # números y viceversa.
     def __init__(self, conversor):
         self.conversor = conversor
         self.baraja = Baraja()
 
+    # Los pasos del algoritmo de crear la clave del solitario
+    # para lo que se indica a un objeto de tipo baraja el
+    # orden y la cantidad de veces (dependiendo de la cantidad
+    # de letras del texto a cifrar/descifrar) que tiene
+    # que ejecutarse.
     def generar_letras_secuencia_de_clave(self, numero_letras):
         letras = []
         while len(letras) < numero_letras:
