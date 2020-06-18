84340a76c8d2a73b4ff03a4b07efb3f14b65bca3
ikermurga <ikermurga@gmail.com>
2020-06-18 22:45:59 +0200

    añadido comentarios

--- tarea_23/conversor_letras_numeros.py
+++ tarea_23/conversor_letras_numeros.py
@@ -1,4 +1,7 @@
 class ConversorLetrasNumeros:
+    # Objeto que contiene una lista de letras
+    # y métodos para pasar de una letra a su
+    # valor numérico y viceversa
     def __init__(self):
         self.letras = [
             'A', 'B', 'C', 'D', 'E',
@@ -9,8 +12,12 @@ class ConversorLetrasNumeros:
             'Z']
 
     def transformar_valor_a_letra(self, valor):
+        # Función que devuelve la letra a partir de un
+        # valor numérico
         return self.letras[valor - 1]
 
     def transformar_letra_a_valor(self, letra):
+        # Función que devuelve un valor numérico a
+        # partir de una letra
         valor = self.letras.index(letra.upper()) + 1
         return valor
