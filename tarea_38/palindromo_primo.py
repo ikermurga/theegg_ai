a01df5b1317416a79079d33a619a8f0a08697c4f
ikermurga <ikermurga@gmail.com>
2020-06-20 19:25:08 +0200

    solución simple

---
+++ tarea_38/palindromo_primo.py
@@ -0,0 +1,25 @@
+def esPrimo(numero):
+    factor = 2
+    while factor < numero/2:
+        if numero % factor == 0:
+            return False
+        factor += 1
+    return True
+
+
+def esPalindromo(numero):
+    numero_texto = str(numero)
+    numero_invertido = numero_texto[::-1]
+    if numero_texto == numero_invertido:
+        return True
+    else:
+        return False
+
+
+numero = int(input("Introduce el número: "))
+while True:
+    if esPalindromo(numero) and esPrimo(numero):
+        print(numero)
+        break
+    print(f'{numero} descartado')
+    numero += 1
