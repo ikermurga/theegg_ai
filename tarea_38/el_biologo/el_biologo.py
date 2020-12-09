ba5af631ac2df41edbca1daec6b06a946a3877ec
ikermurga <ikermurga@gmail.com>
2020-12-09 14:26:07 +0100

    Comentadas funciones

--- tarea_38/el_biologo/el_biologo.py
+++ tarea_38/el_biologo/el_biologo.py
@@ -1,11 +1,12 @@
-# TODO: Comentar cada método y la función principal, tanto en los archivos secundarios como en el principal!
-
 from inputs_cadenas import pedir_inputs
 from extraer_secuencias import obtener_secuencia_comun_mas_larga
 
 
 def main():
     '''
+    El programa de El Biólogo se divide en dos partes principales, por un lado obtener los input del usuario (las cadenas de las bases A, C, G ó T) y por otro encontrar la secuencia común más larga entre ambas. Se han extraido las funciones que corresponden a cada parte a pedir_inputs() y obtener_secuencia_comun_mas_larga(). Por lo tanto esta función obtiene dos cadenas de bases mediante inputs al usuario y después calcula la secuencia común más larga entre ellos.
+
+    Finalmente, mostramos el resultado del cálculo de la secuencia común más larga. Antes de mostrar la cadena más larga que coincide, comprobamos que la cadena no sea una cadena vacía ya que eso indicaría que no hay ningún tipo de secuencia que coincida y mostramos un mensaje correspondiente.
     '''
     secuencias_a_comparar = pedir_inputs()
     secuencia_comun = obtener_secuencia_comun_mas_larga(secuencias_a_comparar)
