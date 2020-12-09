4f68121dbd7c7ce07b5aed7f146d5f30e110dd7d
ikermurga <ikermurga@gmail.com>
2020-12-09 13:54:24 +0100

    Eliminado tareas apuntadas en el código

--- tarea_38/el_biologo/el_biologo.py
+++ tarea_38/el_biologo/el_biologo.py
@@ -1,28 +1,12 @@
-# TODO: diagrama de flujo!
-
-# CASOS PARA TESTS COMIENZAN AQUI
-# texto_secuencia_1 = 'ATGTCTTCCTCGA'
-# texto_secuencia_2 = 'TGCTTCCTATGAC'
-# salida correcta = 'CTTCCT'
-
-# texto_secuencia_1 = 'ctgactga'
-# texto_secuencia_2 = 'actgagc'
-# # salida correcta = actga
-
-# texto_secuencia_1 = 'cgtaattgcgat'
-# texto_secuencia_2 = 'cgtacagtagc'
-# # salida correcta = cgta
-
-# texto_secuencia_1 = 'CTA'
-# texto_secuencia_2 = 'GGG'
-# # salida correcta = No hay secuencia común entre ambas cadenas
-# CASOS PARA TESTS ACABAN AQUI
+# TODO: Comentar cada método y la función principal, tanto en los archivos secundarios como en el principal!
 
 from inputs_cadenas import pedir_inputs
 from extraer_secuencias import obtener_secuencia_comun_mas_larga
 
 
 def main():
+    '''
+    '''
     secuencias_a_comparar = pedir_inputs()
     secuencia_comun = obtener_secuencia_comun_mas_larga(secuencias_a_comparar)
     if len(secuencia_comun) > 1:
