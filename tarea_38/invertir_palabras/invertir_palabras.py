ba9d502647cad6647cfd5aa8ada51cafb0d9ccd2
ikermurga <ikermurga@gmail.com>
2020-12-09 14:44:34 +0100

    Utilizar un while True y break en vez de variable

--- tarea_38/invertir_palabras/invertir_palabras.py
+++ tarea_38/invertir_palabras/invertir_palabras.py
@@ -4,19 +4,19 @@
 # texto = 'foobar'
 texto = 'all your base'
 
-continuar = True
 indice_actual = 0
 indice_espacio = 0
 palabras = []
-# TODO: cambiar a un while True con break
-while continuar:
+
+while True:
     try:
         indice_espacio = texto.index(' ', indice_actual)
         palabras.append(texto[indice_actual: indice_espacio])
         indice_actual = indice_espacio + 1
     except Exception:
         palabras.append(texto[indice_actual:])
-        continuar = False
+        break
+
 palabras_invertidas = palabras[::-1]
 for palabra in palabras_invertidas:
     print(f'{palabra} ', end='')
