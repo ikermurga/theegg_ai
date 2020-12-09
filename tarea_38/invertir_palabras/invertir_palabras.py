a25b6ad881af2d78f65d404e56a7462420031c0e
ikermurga <ikermurga@gmail.com>
2020-12-09 14:46:24 +0100

    Tareas pendientes

--- tarea_38/invertir_palabras/invertir_palabras.py
+++ tarea_38/invertir_palabras/invertir_palabras.py
@@ -2,7 +2,7 @@
 
 # texto = 'this is a test'
 # texto = 'foobar'
-texto = 'all your base'
+texto = 'all your base'  # TODO pedir por input
 
 indice_actual = 0
 indice_espacio = 0
@@ -13,10 +13,12 @@ while True:
         indice_espacio = texto.index(' ', indice_actual)
         palabras.append(texto[indice_actual: indice_espacio])
         indice_actual = indice_espacio + 1
-    except Exception:
+    except Exception:  # TODO la exception especifica
         palabras.append(texto[indice_actual:])
         break
 
-palabras_invertidas = palabras[::-1]
+palabras_invertidas = palabras[::-1]  # TODO usar reverse en vez de esto?
 for palabra in palabras_invertidas:
     print(f'{palabra} ', end='')
+
+# TODO main
