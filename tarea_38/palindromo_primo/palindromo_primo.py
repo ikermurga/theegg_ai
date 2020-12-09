22a4d8123674e180541c808f1ccaa273b909f809
ikermurga <ikermurga@gmail.com>
2020-12-09 20:05:46 +0100

    Extraido a funciones

--- tarea_38/palindromo_primo/palindromo_primo.py
+++ tarea_38/palindromo_primo/palindromo_primo.py
@@ -1,24 +1,39 @@
-def esPrimo(numero):
+def main():
+    numero = obtener_valor_inicial()
+
+    while True:
+        if es_palindromo(numero) and es_primo(numero):
+            print(numero)
+            break
+        numero += 1
+
+
+def es_palindromo(numero):
+    numero_texto = str(numero)
+    numero_invertido = numero_texto[::-1]
+    return numero_texto == numero_invertido
+
+
+def es_primo(numero):
     factor = 2
-    while factor < numero/2:
+    while factor < numero:
         if numero % factor == 0:
             return False
         factor += 1
     return True
 
 
-def esPalindromo(numero):
-    numero_texto = str(numero)
-    numero_invertido = numero_texto[::-1]
-    if numero_texto == numero_invertido:
-        return True
-    else:
-        return False
+def obtener_valor_inicial():
+    while True:
+        try:
+            numero = int(input("Introduce el número: "))
+            if numero < 1:
+                print('Se debe introducir un número mayor a cero.')
+            else:
+                return numero
+        except ValueError:
+            print('Se debe introducir un número entero.')
 
 
-numero = int(input("Introduce el número: "))
-while True:
-    if esPalindromo(numero) and esPrimo(numero):
-        print(numero)
-        break
-    numero += 1
+if __name__ == "__main__":
+    main()
