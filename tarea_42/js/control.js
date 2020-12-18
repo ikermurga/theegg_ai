61ded9fbde9a40553c0d4325f1ebd42d700a3bea
ikermurga <ikermurga@gmail.com>
2020-12-18 03:21:13 +0100

    Añadido selección de idioma de reconocimiento

--- tarea_42/js/control.js
+++ tarea_42/js/control.js
@@ -30,42 +30,133 @@ function lowerPlayerVolume() {
     if (volume < 0) volume = 0;
     player.setVolume(volume);
 }
-var volDownBtn = document.getElementById('vol-down-btn');
-volDownBtn.addEventListener('click', lowerPlayerVolume);
+
 function raisePlayerVolume() {
     var volume = player.getVolume();
     volume += 20;
     if (volume > 100) volume = 100;
     player.setVolume(volume);
 }
-var volUpBtn = document.getElementById('vol-up-btn');
-volUpBtn.addEventListener('click', raisePlayerVolume);
 
 window.SpeechRecognition =
     window.SpeechRecognition || window.webkitSpeechRecognition;
 
 const recognition = new SpeechRecognition();
+recognition.lang = 'es-ES';
 recognition.interimResults = true;
 
+let timesRaised = 0;
+let timesLowered = 0;
 recognition.addEventListener('result', function (event) {
-    // console.log(event);
     const transcript = Array.from(event.results)
         .map((result) => result[0])
         .map((result) => result.transcript)
         .join('');
 
+    changeVolumeDependingOnTranscript(transcript);
+
     if (event.results[0].isFinal) {
-        if (transcript.includes('volume up')) {
-            console.log(transcript);
-            raisePlayerVolume();
-        }
-        if (transcript.includes('volume down')) {
-            console.log(transcript);
-            lowerPlayerVolume();
-        }
+        changeVolumeDependingOnTranscript(transcript);
+
+        timesRaised = 0;
+        timesLowered = 0;
     }
 });
+
+function changeVolumeDependingOnTranscript(transcript) {
+    let volumeUpCommand = '';
+    let volumeDownCommand = '';
+    if (selectedLanguage === 'english') {
+        volumeUpCommand = volumeUpEnglishCommand;
+        volumeDownCommand = volumeDownEnglishCommand;
+    } else if (selectedLanguage === 'spanish') {
+        volumeUpCommand = volumeUpSpanishCommand;
+        volumeDownCommand = volumeDownSpanishCommand;
+    } else if (selectedLanguage === 'basque') {
+        volumeUpCommand = volumeUpBasqueCommand;
+        volumeDownCommand = volumeDownBasqueCommand;
+    }
+    let volumeUpCommands = timesTargetTextIsInText(transcript, volumeUpCommand);
+    let volumeDownCommands = timesTargetTextIsInText(
+        transcript,
+        volumeDownCommand
+    );
+
+    // descomentar para ver lo que está reconociendo el micrófono
+    // console.log(transcript);
+
+    while (timesRaised < volumeUpCommands) {
+        raisePlayerVolume();
+        timesRaised++;
+    }
+
+    while (timesLowered < volumeDownCommands) {
+        lowerPlayerVolume();
+        timesLowered++;
+    }
+}
+
+function timesTargetTextIsInText(text, target) {
+    let times = 0;
+    let index = 0;
+    while (text.includes(target, index)) {
+        times++;
+        index = text.indexOf(target, index) + 1;
+    }
+    return times;
+}
 
 recognition.addEventListener('end', recognition.start);
 
 recognition.start();
+
+document
+    .getElementById('lang-castellano')
+    .addEventListener('change', cambiarOpcionIdioma);
+document
+    .getElementById('lang-ingles')
+    .addEventListener('change', cambiarOpcionIdioma);
+document
+    .getElementById('lang-euskera')
+    .addEventListener('change', cambiarOpcionIdioma);
+
+const instruccionesCastellano = `En castellano, dí
+<strong>volumen arriba</strong> para subir el
+volumen y <strong>volumen abajo</strong> para
+bajarlo.`;
+const instruccionesIngles = `En inglés, dí <strong>volume up</strong> para
+subir el volumen y
+<strong>volume down</strong> para bajarlo.`;
+const instruccionesEuskera = `En euskera, dí <strong>soinua gora</strong> para
+subir el volumen y
+<strong>soinua behera</strong> para bajarlo.`;
+
+const volumeUpEnglishCommand = 'volume up';
+const volumeDownEnglishCommand = 'volume down';
+const volumeUpSpanishCommand = 'volumen arriba';
+const volumeDownSpanishCommand = 'volumen abajo';
+const volumeUpBasqueCommand = 'soinua gora';
+const volumeDownBasqueCommand = 'soinua behera';
+
+let selectedLanguage = 'spanish';
+
+const instruccionesContainer = document.getElementById('instrucciones');
+function cambiarIdiomaA(idioma) {
+    if (idioma === 'castellano') {
+        selectedLanguage = 'spanish';
+        recognition.lang = 'es-ES';
+        instruccionesContainer.innerHTML = instruccionesCastellano;
+    } else if (idioma == 'euskera') {
+        selectedLanguage = 'basque';
+        recognition.lang = 'eu-ES';
+        instruccionesContainer.innerHTML = instruccionesEuskera;
+    } else if (idioma == 'ingles') {
+        selectedLanguage = 'english';
+        recognition.lang = 'en-US';
+        instruccionesContainer.innerHTML = instruccionesIngles;
+    }
+}
+
+function cambiarOpcionIdioma(event) {
+    cambiarIdiomaA(event.target.value);
+}
