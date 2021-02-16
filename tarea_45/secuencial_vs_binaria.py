3bb9d6c4f1b95ea84c8718d5cf234d5f867c70f7
ikermurga <ikermurga@gmail.com>
2021-02-17 00:49:42 +0100

    comentado función main y busqueda_secuencial

--- tarea_45/secuencial_vs_binaria.py
+++ tarea_45/secuencial_vs_binaria.py
@@ -1,17 +1,35 @@
 def main():
+    '''
+    Creamos una lista, la ordenamos (utilizando el ordenamiento de selección) y
+    después buscamos el número 875 en la misma lista que no incluye el número,
+    tanto utilizando la búsqueda secuencial como la binaria.
+
+    Buscar un número no incluido en la lista nos costará el mayor número de 
+    iteraciones por ellas para llegar al resultado y las funciones de
+    busqueda_secuencial y busqueda_binaria devolverán precisamente el
+    número de iteraciones junto con el índice donde se encontró el elemento
+    (o None en caso de no encontrarse) así como  el número de iteraciones
+    para llegar al resultado.
+
+    Finalmente mostramos los datos en consola.
+    '''
     lista = [3, 56, 21, 33, 874, 123, 66, 1000,
              23, 45, 65, 56]
-    elemento_a_buscar = 875
+
     lista_ordenada = ordenamiento_seleccion(lista)
-    print(lista_ordenada)
+    print(f'La lista ordenada es:\n{lista_ordenada}\n')
+
+    elemento_a_buscar = 875
     indice_secuencial, iteraciones_secuencial = busqueda_secuencial(
         lista_ordenada, elemento_a_buscar)
     indice_binaria, iteraciones_binaria = busqueda_binaria(
         lista_ordenada, elemento_a_buscar)
+
     print(
         f'El indice del elemento {elemento_a_buscar} resultante de la búsqueda secuencial es el {indice_secuencial}')
     print(
         f'El número de interaciones de la búsqueda secuencial han sido: {iteraciones_secuencial}')
+    print()
     print(
         f'El indice del elemento {elemento_a_buscar} resultante de la búsqueda secuencial es el {indice_binaria}')
     print(
@@ -19,6 +37,13 @@ def main():
 
 
 def busqueda_secuencial(lista, item):
+    '''
+    Recorre la lista elemento a elemento hasta que encuentra
+    el item recibido. Antes de comprobar cada elemento se
+    añade 1 a la variable de iteraciones, que se devolverá
+    junto con el índice donde se encontró el item en la
+    lista (o None en caso de que item no sea parte de la lista)
+    '''
     iteraciones = 0
     for indice, element in enumerate(lista):
         iteraciones += 1
@@ -28,6 +53,9 @@ def busqueda_secuencial(lista, item):
 
 
 def busqueda_binaria(lista, item):
+    '''
+
+    '''
     iteraciones = 0
     bajo = 0
     alto = len(lista) - 1
@@ -45,21 +73,19 @@ def busqueda_binaria(lista, item):
 
 
 def ordenamiento_seleccion(lista):
+    '''
+
+    '''
     lista_ordenada = []
     for _ in range(len(lista)):
-        indice_menor = encontrar_menor(lista)
+        menor = lista[0]
+        indice_menor = 0
+        for indice in range(1, len(lista)):
+            if lista[indice] < menor:
+                menor = lista[indice]
+                indice_menor = indice
         lista_ordenada.append(lista.pop(indice_menor))
     return lista_ordenada
-
-
-def encontrar_menor(lista):
-    menor = lista[0]
-    indice_menor = 0
-    for indice in range(1, len(lista)):
-        if lista[indice] < menor:
-            menor = lista[indice]
-            indice_menor = indice
-    return indice_menor
 
 
 if __name__ == '__main__':
